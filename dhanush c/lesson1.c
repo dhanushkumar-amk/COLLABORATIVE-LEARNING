b31031256792a62e97279f9566b0ec34afbe1b13
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2026-09-13 10:40:28 +0530

    feat: add lesson1.c with basic print statements and arithmetic

---
+++ dhanush c/lesson1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    // Print a greeting message to the console
+    printf("Hello, World!\n");
+    printf("\n");
+    printf("%d\n", 10 + 5);
+
+    return 0;
+}
